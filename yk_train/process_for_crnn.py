c7d40e4d24b8586d3e6c239e4e1a23a834c7dc7a
Yang Ke <221900275@smail.nju.edu.cn>
2023-12-15 13:11:20 +0800

    Add files via upload
    增加了split_num，扩大数据集

--- yk_train/process_for_crnn.py
+++ yk_train/process_for_crnn.py
@@ -6,9 +6,9 @@ import sklearn.model_selection as skms
 import torch
 
 
-def compute_melgram(audio_path):
+def compute_melgram(audio_path, split_num=1):
     '''
-    return a mel-spectrogram in shape of (96, 1366)
+    return a mel-spectrogram in shape of [(96, 1366) * split_number]
     '''
 
     SR = 12000
@@ -18,20 +18,27 @@ def compute_melgram(audio_path):
     DURA = 29.12  # to make it 1366 frame..
 
     src, sr = librosa.load(audio_path, sr=SR)  # whole signal
-    n_sample = src.shape[0]
+    n_sample = src.shape[0] // split_num
     n_sample_fit = int(DURA * SR)
 
-    if n_sample < n_sample_fit:  # if too short
-        src = np.hstack((src, np.zeros((int(DURA * SR) - n_sample,))))
-    elif n_sample > n_sample_fit:  # if too long
-        src = src[(n_sample - n_sample_fit) // 2:(n_sample + n_sample_fit) // 2]
-    logam = librosa.amplitude_to_db
-    melgram = librosa.feature.melspectrogram
-    ret = logam(melgram(y=src, sr=SR, hop_length=HOP_LEN, n_fft=N_FFT, n_mels=N_MELS), ref=np.max)
-    ret = ret[np.newaxis, :]
-    return ret
+    mels = []
+    for i in range(split_num):
+        start = i * n_sample
+        end = start + n_sample
+        s = src[start:end]
+        if n_sample < n_sample_fit:  # if too short
+            s = np.hstack((s, np.zeros((int(DURA * SR) - n_sample,))))
+        elif n_sample > n_sample_fit:  # if too long
+            s = s[(n_sample - n_sample_fit) // 2:(n_sample + n_sample_fit) // 2]
+        logam = librosa.amplitude_to_db
+        melgram = librosa.feature.melspectrogram
+        ret = logam(melgram(y=s, sr=SR, hop_length=HOP_LEN, n_fft=N_FFT, n_mels=N_MELS), ref=np.max)
+        ret = ret[np.newaxis, :]
+        mels.append(ret)
 
-def data_read(directory=r"..\..\dataset\archive\Data\genres_original"):
+    return mels
+
+def data_read(directory=r"..\..\dataset\archive\Data\genres_original", split_num=1):
     classes = {'blues': 0,
                'classical': 1,
                'country': 2,
@@ -46,35 +53,31 @@ def data_read(directory=r"..\..\dataset\archive\Data\genres_original"):
     mels = []
     labels = []
 
-    x = 0
-
     for dirname, _, filenames in os.walk(directory):
         for filename in filenames:
             if filename == "jazz.00054.wav":  # skip the broken one
-                x += 1
                 continue
             filename = os.path.join(dirname, filename)
-            mel = compute_melgram(filename)
+            mel = compute_melgram(filename, split_num)
             # TODO "数字指向类别，如'..\\blue\\blue0000.wav', 数字应为1"
             label = classes[filename.split('\\')[6]]
 
-            mels.append(mel)
-            labels.append(label)
-            x += 1
-
+            mels.extend(mel)
+            for i in range(split_num):
+                labels.append(label)
 
     return mels, labels
 
 class GTZANDataset_CRNN():
-    def __init__(self, rootDir=r"..\..\dataset\archive\Data\genres_original"):
-        mels, labels = data_read(rootDir)
+    def __init__(self, rootDir=r"..\..\dataset\archive\Data\genres_original", split_num=1):
+        mels, labels = data_read(rootDir, split_num)
         mels = np.array(mels)
         labels = np.array(labels)
-        print(mels.shape)
+        # print(mels.shape)
         mels_train, mels_test, labels_train, labels_test = skms.train_test_split(mels, labels, test_size=0.3, random_state=0)
         mels_train = torch.Tensor(mels_train)
         mels_test = torch.Tensor(mels_test)
-        print(mels_train.shape)
+        # print(mels_train.shape)
         labels_train = torch.Tensor(labels_train)
         labels_test = torch.Tensor(labels_test)
 
@@ -96,7 +99,8 @@ if __name__ == "__main__":
     # mel = compute_melgram(r"..\..\dataset\archive\Data\genres_original\blues\blues.00000.wav")
     # print(mel)
     # print(mel.shape)
-
-    dataset = GTZANDataset_CRNN()
+    # data_read(split_num=10)
+    dataset = GTZANDataset_CRNN(split_num=10)
     print(dataset.trainDataset[0])
+    print(len(dataset.trainDataset))
 
